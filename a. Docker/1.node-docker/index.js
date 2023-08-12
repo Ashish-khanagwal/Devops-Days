cc4a990c0fb9df8d614e9aef109fb841bea5be2d
Ashish Khanagwal <ashishkhanagwal2001@gmail.com>
2023-08-12 22:40:30 +0530

    MODIFIED: changes in some files

--- a. Docker/1.node-docker/index.js
+++ a. Docker/1.node-docker/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const redis = require("redis");
+const session = require("express-session");
+let RedisStore = require("connect-redis").default;
+
 const {
   MONGO_USER,
   MONGO_PASSWORD,
   MONGO_IP,
   MONGO_PORT,
+  REDIS_URL,
+  REDIS_PORT,
+  SESSION_SECRET,
 } = require("./config/config");
 
-const postRouter = require("./routes/postRoutes");
-const userRouter = require("./routes/userRoutes");
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
 
 const app = express();
 
@@ -17,7 +26,7 @@ const connectWithRetry = () => {
     .connect(
       `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
     )
-    .then(() => console.log("Succesfully connected to DB"))
+    .then(() => console.log("Successfully connected to DB"))
     .catch((e) => {
       console.log(e);
       setTimeout(connectWithRetry, 5000);
@@ -26,14 +35,26 @@ const connectWithRetry = () => {
 
 connectWithRetry();
 
+app.use(
+  session({
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000,
+    },
+  })
+);
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h2>Hello World</h2>");
 });
 
-app.use("/api/v1/posts", postRouter);
-app.use("/api/v1/users", userRouter);
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`listening on port ${port}`));
+app.listen(3000, () => console.log("Listening on port 3000"));
