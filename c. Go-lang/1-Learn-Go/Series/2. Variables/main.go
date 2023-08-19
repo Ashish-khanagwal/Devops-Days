0b4ab12aa4be38e4bd9e111279ba46c269c30741
Ashish Khanagwal <ashishkhanagwal2001@gmail.com>
2023-08-19 23:50:56 +0530

    Variables course added

--- c. Go-lang/1-Learn-Go/Series/2. Variables/main.go
+++ c. Go-lang/1-Learn-Go/Series/2. Variables/main.go
@@ -16,4 +16,13 @@ func main() {
 	var rollNo uint8 = 255
 	fmt.Println(rollNo)
 	fmt.Printf("Data type of rollNo is: %T \n", rollNo)
+
+	var marks float32 = 95.869789
+	fmt.Println(marks)
+	fmt.Printf("Data type of marks is: %T \n", marks)
+
+	var isPass bool = true
+	fmt.Println(isPass)
+	fmt.Printf("Data type of isPass is: %T \n", isPass)
+
 }
