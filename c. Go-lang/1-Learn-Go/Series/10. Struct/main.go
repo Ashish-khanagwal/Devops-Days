ed582ddf025aab429ab59d339c498279914453fe
Ashish Khanagwal <ashishkhanagwal2001@gmail.com>
2023-09-02 23:26:56 +0530

    Struct ended

--- c. Go-lang/1-Learn-Go/Series/10. Struct/main.go
+++ c. Go-lang/1-Learn-Go/Series/10. Struct/main.go
@@ -6,6 +6,15 @@ import (
 	"os"
 )
 
+// This is how we define a struct : To define a struct, use
+// the type keyword followed by the struct name and a set of fields enclosed in curly braces.
+type User struct { // One thing to be noticed that struct name should always start with uppercase letter
+	FirstName, LastName string
+	Age                 int
+	Email               string
+	Location            string
+}
+
 func main() {
 	fmt.Println("Let's learn about Struct")
 
@@ -13,4 +22,26 @@ func main() {
 	fmt.Println("What we are gonna learn today? : ")
 	input, _ := Reader.ReadString('\n')
 	fmt.Println("We will be learning: ", input)
+
+	user := User{"Ashish", "Khanagwal", 22, "ashish@gmail.com", "Delhi"}
+	fmt.Println(user)
+	fmt.Printf("User details are: %+v\n", user) // %+v will provide the full details about the struct
+	fmt.Printf("User's name is %v and email is %v\n", user.FirstName, user.Email)
+
+	// We can also define anonymous struct, see below
+	// This is useful when we want to create a struct for one time use
+	person := struct {
+		Name           string
+		age            int
+		profession     string
+		registrationId uint
+	}{
+		Name:           "Ashish",
+		age:            22,
+		profession:     "DevOps Engineer",
+		registrationId: 032,
+	}
+
+	fmt.Println("This is how our anonymous struct will look like: ", person)
+	fmt.Println(person.Name)
 }
