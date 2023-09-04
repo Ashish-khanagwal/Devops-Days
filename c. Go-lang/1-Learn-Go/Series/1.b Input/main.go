07c9cc46a30c5b673dcd27833e66d7073b5838c2
Ashish Khanagwal <ashishkhanagwal2001@gmail.com>
2023-09-04 22:54:23 +0530

    input in go demystified

---
+++ c. Go-lang/1-Learn-Go/Series/1.b Input/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	// "bufio"
+	"fmt"
+	// "os"
+)
+
+func main() {
+	fmt.Println("Now, different methods of taking inputs")
+
+	// Method 1
+	var num int
+	fmt.Println("Enter a number: ")
+	_, err := fmt.Scan(&num)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println("you entered: ", num)
+}
--- c. Go-lang/1-Learn-Go/Series/11. Conditionals/main.go
+++ c. Go-lang/1-Learn-Go/Series/11. Conditionals/main.go
@@ -27,15 +27,25 @@ func main() {
 		fmt.Println("You can vote")
 	} // This is a basic demonstration how do we use if statement, Also we have else. Let's see
 
-
-	age int := bufio.NewReader(os.Stdin)
+	var age int
 	fmt.Println("Enter your age: ")
-	input, _ := age.ReadString('\n')
-	fmt.Println("Your age is: ", input)
-
-	if age > 18 {
+	_, err := fmt.Scan(&age)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	} else if age >= 18 {
+		fmt.Println("Your age is: ", age)
 		fmt.Println("You can travel via plane")
 	} else {
 		fmt.Println("Sorry, you can't travel via plane")
 	}
+
+	var height, weight int
+	fmt.Println("Enter your height (in cm) and weight (in kg): ")
+	_, err = fmt.Scan(&height, &weight)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println("your height and weight is: ", height, weight)
 }
