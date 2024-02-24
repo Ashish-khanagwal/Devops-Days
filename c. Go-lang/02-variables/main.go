aa5e0688401970c05593b38b787d8840335e56e0
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2024-02-24 23:12:17 +0530

    Modified: golang file

--- c. Go-lang/02-variables/main.go
+++ c. Go-lang/02-variables/main.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "strconv"
+)
 
 func main() {
 	var username = "Ashish"
 	fmt.Println("username: ",username)
 	fmt.Printf("The data type of username is: %T \n", username)
+
+	var myname = "Ashish"
+	fmt.Println("Your name is: " + myname)
+	fmt.Printf("Data type of myname is: %T \n", myname)
+	
+	// var age int
+	// age = 30
+	// fmt.Println("Your age is: " + strconv.Itoa(age))
+	
+	var age int = 30
+	fmt.Println(age)
+	fmt.Printf("Data type of age is: %T \n", age)
+
 }
