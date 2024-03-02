0df9fb13cb47e529014082164ffc8093625eb2d1
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2024-03-02 13:13:39 +0530

    CHANGED: file hierarchy

--- c. Go-lang/01-LEARN-GO/02-variables/main.go
+++ c. Go-lang/01-LEARN-GO/02-variables/main.go
@@ -7,17 +7,17 @@ import (
 
 func main() {
 	var username = "Ashish"
-	fmt.Println("username: ",username)
+	fmt.Println("username: ", username)
 	fmt.Printf("The data type of username is: %T \n", username)
 
 	var myname = "Ashish"
 	fmt.Println("Your name is: " + myname)
 	fmt.Printf("Data type of myname is: %T \n", myname)
-	
+
 	// var age int
 	// age = 30
 	// fmt.Println("Your age is: " + strconv.Itoa(age))
-	
+
 	var age int = 30
 	fmt.Println(age)
 	fmt.Printf("Data type of age is: %T \n", age)
