52fab316f42503998b2b8f10bb44831e1050e27b
Ashish-khangagwal <ashishkhanagwal2001@gmail.com>
2025-08-15 17:22:46 +0530

    Lists in mid

--- b. programming-language/Python/4. Lists/main.py
+++ b. programming-language/Python/4. Lists/main.py
@@ -2,3 +2,59 @@ line = "***********************"
 print(line)
 print("Lists".center(23))
 print(line)
+
+# List is mutable, ordered, and can store mixed data types.
+fruits = [
+    "Apple",
+    "Banana",
+    "mango",
+    "Grapes",
+    "Strawberry",
+    "Pineapple",
+    "Guvava",
+    "Dragonfruit",
+]
+print(fruits)
+
+print(fruits[1])
+mixed = ["Ashish", 24, 1, 1.79]
+print(mixed[2])
+print(mixed[-1])
+print("Ashish" in mixed)
+
+print(fruits.index("Pineapple"))
+
+# Slicing in lists
+print(fruits[1:4])
+
+num = ["Ashish", 4, 6, 8, 10, 12, 14, 16, 18, 20]
+print(num[5])
+print(num.index(16))
+print(num[2:7])
+
+# Changing the item
+fruits[2] = "Peach"
+print(fruits)
+print(len(fruits))
+print("")
+
+# Append -> Add item at the end
+fruits.append("Orange")
+print(fruits)
+print(len(fruits))
+print("")
+
+# Add item at a specific index
+fruits.insert(2, "pomegranate")
+print(fruits)
+print(len(fruits))
+print("")
+
+# Extend -> to add multiple items
+fruits.extend(["Watermelon", "blueberry"])
+print(fruits)
+print(len(fruits))
+
+fruits.extend(num)
+print(fruits)
+print(len(fruits))
---
+++ b. programming-language/Python/4. Lists/methods.py
@@ -0,0 +1,53 @@
+users = ["Ashish", "Manish", "Saurav", "Harsh", "Lalit", "Aniket"]
+print(users)
+print(len(users))
+
+users[4:4] = [
+    "Robert",
+    "Alex",
+]  # this will add these values at index 4 without replacing
+print(users)
+
+users[4:6] = ["Eddie", "RDJ"]  # This will replace the values from index 4 to 5
+print(users)
+print("")
+
+# Removing the item from the lists
+users.remove("Ashish")  # --> Remove the mentioned item from the list
+print(users)
+print("")
+
+print(users.pop())  # --> Will return the last element and removes it too
+print(users)  # --> Removes the last element from the list
+print("")
+
+del users[0]  # --> Deletes the item from the list at index 0
+print(users)
+print("")
+
+# users.clear()  # --> Clears this list and return the empty list
+# print(users)
+
+num = [3, 1, 4, 7, 5, 2, 8, 6, 9, 3, 1, 3]
+print(len(num))
+print(min(num))
+print(max(num))
+print(sum(num))
+print(num.count(3))
+print(num.index(3))
+
+num.sort()
+print(num)
+
+num.sort(reverse=True)
+print(num)
+
+num.reverse()
+print(num)
+
+users[0:0] = ["ashish"]
+users.sort()
+print(users)
+
+users.sort(key=str.lower)
+print(users)
